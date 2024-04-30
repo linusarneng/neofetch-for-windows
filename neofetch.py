797c1aeb43ba1908b8dcb80f2a9823457b3952bf
𝓛𝓲𝓷𝔁 <linus.arneng@gmail.com>
2024-04-30 23:00:55 +0200

    Create neofetch.py

---
+++ neofetch.py
@@ -0,0 +1,23 @@
+# Neofetch cmd but for windows
+
+print("""                    \033[92m....,,:;+ccllll    \033[00m  
+      \033[91m...,,+:;  \033[92mcllllllllllllllllll\033[0m    ---------
+\033[91m,cclllllllllll  \033[92mllllllllllllllllll\033[0m    \033[94mOS: \033[0mwindows 11 pro x86_64
+\033[91mllllllllllllll\033[0m  \033[92mllllllllllllllllll\033[0m    \033[94mHost: \033[0mASUSTeK COMPUTER INC. Vivobook 15 ASUS Laptop X570UD
+\033[91mllllllllllllll\033[0m  \033[92mllllllllllllllllll\033[0m    \033[94mKernel: \033[0m10.0.22000
+\033[91mllllllllllllll\033[0m  \033[92mllllllllllllllllll\033[0m    \033[94mUptime: \033[0m2 days, 5 hours, 54 mins
+\033[91mllllllllllllll\033[0m  \033[92mllllllllllllllllll\033[0m    \033[94mPackages: \033[0m4 (scoop)
+\033[91mllllllllllllll\033[0m  \033[92mllllllllllllllllll\033[0m    \033[94mShell: \033[0mbash 4.4.23
+                                      \033[94mResolution: \033[0m1920x1080
+\033[94mllllllllllllll\033[0m  \033[93mllllllllllllllllll\033[0m    \033[94mDE: \033[0mAero
+\033[94mllllllllllllll\033[0m  \033[93mllllllllllllllllll\033[0m    \033[94mWM: \033[0mExplorer            
+\033[94mllllllllllllll\033[0m  \033[93mllllllllllllllllll\033[0m    \033[94mVM \033[0mTheme: themeA
+\033[94mllllllllllllll\033[0m  \033[93mllllllllllllllllll\033[0m    \033[94mCPU: \033[0mIntel i7-8550U (8) @ 2.000GHz
+\033[94mllllllllllllll\033[0m  \033[93mllllllllllllllllll\033[0m    \033[94mGPU: \033[0mNVIDIA GeForce GTX 3060 Ti
+\033[94m`'ccllllllllll  \033[93mlllllllllllllllllll   \033[94mMemory: \033[0m7411MiB / 12166MiB
+       \033[94m`' \\*::  \033[93m:ccllllllllllllllll      
+                       ````''*::cll\033[00m   \033[90m██\033[31m██\033[32m██\033[33m██\033[34m██\033[35m██\033[96m██\033[97m██        
+                                 \033[93m``\033[0m
+      
+      """)
+
